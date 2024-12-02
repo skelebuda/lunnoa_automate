a99c5cafafdce138b8c741a6215b55d02665dca9
Tony Ramirez <tonyramirezlecca@gmail.com>
2024-12-02 12:46:36 -0700

    feat(server): Add provider endpoint to ai controller

--- apps/server/src/modules/global/ai-provider/ai-provider.controller.ts
+++ apps/server/src/modules/global/ai-provider/ai-provider.controller.ts
@@ -1,5 +1,7 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+
+import { Public } from '@/decorators/public.decorator';
 
 import { AiProviderService } from './ai-provider.service';
 
@@ -8,4 +10,10 @@ import { AiProviderService } from './ai-provider.service';
 @ApiBearerAuth()
 export class AiProviderController {
   constructor(private readonly aiProviderService: AiProviderService) {}
+
+  @Get('providers')
+  @Public()
+  async getSubscriptionProducts() {
+    return this.aiProviderService.providers;
+  }
 }
