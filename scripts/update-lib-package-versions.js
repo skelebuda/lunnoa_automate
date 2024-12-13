14f8d401e1af808311148908d7c19e9f2e953f10
Tony Ramirez <tonyramirezlecca@gmail.com>
2024-12-13 16:29:41 -0700

    fix: Use semantic-release/exec to update lib package.json

---
+++ scripts/update-lib-package-versions.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+
+// Get the new version from command line arguments
+const newVersion = process.argv[2];
+
+if (!newVersion) {
+  console.error('No version provided');
+  process.exit(1);
+}
+
+// List of package.json files to update
+const packagePaths = [
+  'packages/toolkit/package.json',
+  'packages/apps/package.json',
+];
+
+// Update each package.json
+packagePaths.forEach((packagePath) => {
+  const fullPath = path.join(__dirname, '..', packagePath);
+
+  try {
+    // Read the package.json
+    const packageJson = JSON.parse(fs.readFileSync(fullPath, 'utf8'));
+
+    // Update the version
+    packageJson.version = newVersion;
+
+    // Write back to file
+    fs.writeFileSync(fullPath, JSON.stringify(packageJson, null, 2) + '\n');
+
+    console.info(`Updated ${packagePath} to version ${newVersion}`);
+  } catch (error) {
+    console.error(`Error updating ${packagePath}:`, error);
+    process.exit(1);
+  }
+});
