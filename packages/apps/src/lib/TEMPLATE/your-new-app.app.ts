6d06ae5188c95c628d5ab2e7e07da3bd41f637f1
Tony Ramirez <tonyramirezlecca@gmail.com>
2024-12-17 19:21:45 -0700

    refactor(apps): Update template app files

---
+++ packages/apps/src/lib/TEMPLATE/your-new-app.app.ts
@@ -0,0 +1,12 @@
+import { createApp } from '@lecca-io/toolkit';
+
+export const yourNewApp = createApp({
+  id: 'your-new-app',
+  name: '',
+  description: '',
+  logoUrl:
+    'https://lecca-io.s3.us-east-2.amazonaws.com/assets/apps/your-new-app.svg',
+  actions: [],
+  triggers: [],
+  connections: [],
+});
