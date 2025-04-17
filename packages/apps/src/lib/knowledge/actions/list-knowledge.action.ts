6f76543f991b30ce3eaf202d1dbc3000a6faae19
skelebuda <sasakelebuda@lunnoalabs.ch>
2025-04-17 14:28:47 +0200

    feat: fix knowledge import

--- packages/apps/src/lib/knowledge/actions/list-knowledge.action.ts
+++ packages/apps/src/lib/knowledge/actions/list-knowledge.action.ts
@@ -1,4 +1,4 @@
-import { createAction, createMarkdownField } from '@lecca-io/toolkit';
+import { createAction, createMarkdownField } from '@lunnoa-automate/toolkit';
 import { z } from 'zod';
 
 export const listKnowledge = createAction({
