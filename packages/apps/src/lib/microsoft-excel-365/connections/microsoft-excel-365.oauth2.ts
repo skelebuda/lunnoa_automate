ef2c47501c85dd1d6a42b53cb1d6de942b21e931
skelebuda <sasakelebuda@lunnoalabs.ch>
2025-05-15 12:56:12 +0200

    feat: excel and outlook integration

--- packages/apps/src/lib/microsoft-excel-365/connections/microsoft-excel-365.oauth2.ts
+++ packages/apps/src/lib/microsoft-excel-365/connections/microsoft-excel-365.oauth2.ts
@@ -7,9 +7,9 @@ export const microsoftExcel365OAuth2 = createOAuth2Connection({
   authorizeUrl:
     'https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
   tokenUrl: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
-  getClientId: () => process.env.INTEGRATION_MICROSOFT_365_EXCEL_CLIENT_ID,
+  getClientId: () => process.env.INTEGRATION_MICROSOFT_365_GRAPH_CLIENT_ID,
   getClientSecret: () =>
-    process.env.INTEGRATION_MICROSOFT_365_EXCEL_CLIENT_SECRET,
+    process.env.INTEGRATION_MICROSOFT_365_GRAPH_CLIENT_SECRET,
   scopes: ['Files.ReadWrite', 'User.Read', 'offline_access'],
   redirectToLocalHostInDevelopment: true,
 });
