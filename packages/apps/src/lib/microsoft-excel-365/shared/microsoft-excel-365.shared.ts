8881132e5cdc355fcb3cdf07647fb3e259922079
skelebuda <sasakelebuda@lunnoalabs.ch>
2025-05-15 22:39:07 +0200

    feat: add row to worksheet

--- packages/apps/src/lib/microsoft-excel-365/shared/microsoft-excel-365.shared.ts
+++ packages/apps/src/lib/microsoft-excel-365/shared/microsoft-excel-365.shared.ts
@@ -1,4 +1,4 @@
-import { createDynamicSelectInputField } from '@lunnoa-automate/toolkit';
+import { createDynamicSelectInputField, createNumberInputField, FieldConfig } from '@lunnoa-automate/toolkit';
 
 
 export const shared = {
@@ -34,5 +34,75 @@ export const shared = {
         missingMessage: 'Workbook is required',
       },
     }),
+    
+    headerRowNumber: createNumberInputField({
+      id: 'headerRowNumber',
+      label: 'Header Row Number',
+      description: 'The row number that contains the headers',
+      defaultValue: 1,
+      required: {
+        missingStatus: 'warning',
+        missingMessage: 'Header row number is required',
+      },
+    }),
+    
+    dynamicSelectHeadersMap: {
+      id: 'mappings',
+      label: 'Column Mappings',
+      description: 'Map values to columns',
+      inputType: 'map',
+      mapOptions: {
+        keyLabel: 'Column',
+        valueLabel: 'Value',
+        keyPlaceholder: 'Select column',
+        valuePlaceholder: 'Enter value',
+      },
+      _getDynamicKeys: async ({ connection, workspaceId, http, extraOptions }) => {
+        if (!extraOptions?.workbookId || !extraOptions?.worksheetId || !extraOptions?.headerRowNumber) {
+          return [];
+        }
+        
+        const headerRow = extraOptions.headerRowNumber;
+        const url = `https://graph.microsoft.com/v1.0/me/drive/items/${extraOptions.workbookId}/workbook/worksheets/${extraOptions.worksheetId}/range(address='${headerRow}:${headerRow}')`;
+        
+        const response = await http.request({
+          method: 'GET',
+          url,
+          headers: {
+            Authorization: `Bearer ${connection.accessToken}`,
+          },
+          workspaceId,
+        });
+        
+        if (!response.data.values || !response.data.values[0]) {
+          return [];
+        }
+        
+        return response.data.values[0].map((header: string, index: number) => ({
+          value: header,
+          label: header,
+        }));
+      },
+      loadOptions: {
+        dependsOn: [
+          {
+            id: 'workbookId',
+            value: ''
+          },
+          {
+            id: 'worksheetId',
+            value: ''
+          },
+          {
+            id: 'headerRowNumber',
+            value: ''
+          },
+        ],
+      },
+      required: {
+        missingStatus: 'warning',
+        missingMessage: 'At least one column mapping is required',
+      },
+    } as FieldConfig,
   },
 };
