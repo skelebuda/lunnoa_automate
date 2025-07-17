da3ebf5b6f20af641275ef2e778da0f8ffedaf82
skelebuda <sasakelebuda@lunnoalabs.ch>
2025-07-17 16:07:50 +0200

    feat: add agent actions 20250717140737_added_table_back

---
+++ packages/server/prisma/migrations/20250717140737_added_table_back/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "AgentAction" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+    "actionId" TEXT NOT NULL,
+    "FK_agentId" TEXT NOT NULL,
+
+    CONSTRAINT "AgentAction_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "AgentAction" ADD CONSTRAINT "AgentAction_FK_agentId_fkey" FOREIGN KEY ("FK_agentId") REFERENCES "Agent"("id") ON DELETE CASCADE ON UPDATE CASCADE;
